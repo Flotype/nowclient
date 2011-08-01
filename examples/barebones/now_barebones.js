a7896a8a908a3fde2430322b3a5000c0af4cabe1
Steve Wang <steve.john.wang@gmail.com>
2011-08-01 16:49:14 -0700

    Updated nowclient to use newer proxy and scopeTable methods.

--- examples/barebones/now_barebones.js
+++ examples/barebones/now_barebones.js
@@ -2,6 +2,8 @@ var nowclient = require('nowclient');
 var now = nowclient.now;
 var socket = nowclient.socket;
 
-now.receiveMessage = function (message) {
-  console.log(message);
-};
+now.ready(function () {
+  now.receiveMessage = function (name, message) {
+    console.log(name, ':', message);
+  };
+});
